f486c00a48d889caaac40e7c898d9b6a4c0e1527
Niguel Price <niguelprice@yahoo.com>
2023-01-24 09:11:21 -0500

    added link to connect registerpage with my searchresultspage

--- ReactDjango_JWT_StarterCode-main/frontend/src/pages/SearchResultsPage/SearchResultsPage.js
+++ ReactDjango_JWT_StarterCode-main/frontend/src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+
 const SearchResultsPage = () => {
-    return ( <h1>hello</h1> );
+
+
+
+    return ( <h1>hello
+        <Link to="/register">Click to register!</Link>
+    </h1> );
 }
  
 export default SearchResultsPage;
